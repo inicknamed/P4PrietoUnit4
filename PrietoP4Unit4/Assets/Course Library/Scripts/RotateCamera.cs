d111fbb2a74bf24c9834576b7850513765abb60b
Damien <756225@student.dusd.net>
2026-04-20 11:14:47 -0700

    wew
    ewewe

--- PrietoP4Unit4/Assets/Course Library/Scripts/RotateCamera.cs
+++ PrietoP4Unit4/Assets/Course Library/Scripts/RotateCamera.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     public float rotationSpeed = 1f;
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
